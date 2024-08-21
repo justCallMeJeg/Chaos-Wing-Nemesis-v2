c3ed927b521a4c72adf56c6c855b8049922d7bb3
Rover <87892483+justCallMeJeg@users.noreply.github.com>
2024-08-21 15:46:27 +0800

    Created main anim handler

--- src/Scripts/Scenes/GameSideSelection.gd
+++ src/Scripts/Scenes/GameSideSelection.gd
@@ -1,14 +1,10 @@
 extends Control
 
-@onready var side_border_ui = $SideBorderUI
-@onready var side_tooltip_ui = $SideTooltipUi
-@onready var main_text_ui = $MainTextUi
+@onready var animPlayer = $AnimPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("[DEBUG]: GameSideSelection scene ready...")
 	GameManager.setGameState(GameManager.GameStates.SideSelection)
 	print("[DEBUG]: GameState set to 'SideSelection'")
-	side_border_ui.playIntroAnim()
-	side_tooltip_ui.playIntroAnim()
-	main_text_ui.playIntroAnim()
+	animPlayer.play("IntroAnim")
