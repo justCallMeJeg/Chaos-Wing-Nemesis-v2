e18d87bd4d35fa9cbc703df2114ff1ae13a22f84
Rover <87892483+justCallMeJeg@users.noreply.github.com>
2024-08-22 15:41:12 +0800

    Updated sideSelection script

--- src/Scripts/Scenes/GameSideSelection.gd
+++ src/Scripts/Scenes/GameSideSelection.gd
@@ -5,6 +5,15 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("[DEBUG]: GameSideSelection scene ready...")
-	GameManager.setGameState(GameManager.GameStates.SideSelection)
-	print("[DEBUG]: GameState set to 'SideSelection'")
 	animPlayer.play("IntroAnim")
+	
+func _input(event) -> void:
+	if event is InputEventKey and event.pressed:
+		match event.keycode:
+			KEY_SPACE:
+				if GameManager.currentGameState == GameManager.GameStates.SideSelection:
+					GameManager.currentGameState = GameManager.GameStates.ShipSelection
+					get_tree().change_scene_to_file("res://src/Scenes/GameStates/GameShipSelection.tscn")
+				else:
+					GameManager.currentGameState = GameManager.GameStates.SideSelection
+					get_tree().change_scene_to_file("res://src/Scenes/GameStates/GameSideSelection.tscn")
