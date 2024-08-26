3062b5da8951f20c3094639c306990d0167ad572
gjpgabayeron <gjpgabayeron@gmail.com>
2024-08-26 16:21:58 +0800

    Added UIText util support; scene handlers

--- src/Scripts/Scenes/UI/SideTooltipUI.gd
+++ src/Scripts/Scenes/UI/SideTooltipUI.gd
@@ -4,11 +4,50 @@ extends Control
 @onready var bottomText = $BottomTooltipContainer/Margin/Text
 @onready var animPlayer = $AnimPlayer
 
-func setText(_str: String, side: int) -> void:
-	if side == 0:
+var UIText = UIUtil.Text["SelectionScene"].SideTooltipUI
+
+func _ready() -> void:
+	if GameManager.currentGameState == GameManager.GameStates.SideSelection:
+		setText(UIText.SideSelection.P1TooltipText, GameManager.PlayableSides.TOP)
+		setText(UIText.SideSelection.P2TooltipText, GameManager.PlayableSides.BOTTOM)
+	elif GameManager.currentGameState == GameManager.GameStates.ShipSelection:
+		if GameManager.currentP1Side == GameManager.PlayableSides.TOP:
+			setText(UIText.SideSelection.P1TooltipText, GameManager.PlayableSides.TOP)
+			setText(UIText.SideSelection.P2TooltipText, GameManager.PlayableSides.BOTTOM)
+		elif GameManager.currentP1Side == GameManager.PlayableSides.BOTTOM:
+			setText(UIText.SideSelection.P2TooltipText, GameManager.PlayableSides.TOP)
+			setText(UIText.SideSelection.P1TooltipText, GameManager.PlayableSides.BOTTOM)
+		else:
+			setText(UIText.SideSelection.P1TooltipText, GameManager.PlayableSides.TOP)
+			setText(UIText.SideSelection.P2TooltipText, GameManager.PlayableSides.BOTTOM)
+
+func setText(_str: String, side: GameManager.PlayableSides) -> void:
+	if side == GameManager.PlayableSides.TOP:
 		topText.text = _str
-	else:
+	elif side == GameManager.PlayableSides.BOTTOM:
 		bottomText.text = _str
+
+func sceneTooltipHandler() -> void:
+	var tooltipText = UIText
+	
+	if GameManager.currentGameState == GameManager.GameStates.SideSelection:
+		if GameManager.P1Selected and GameManager.P2Selected:
+			setText(tooltipText.SideSelection.IdleText, GameManager.PlayableSides.TOP)
+			setText(tooltipText.SideSelection.IdleText, GameManager.PlayableSides.BOTTOM)
+		elif GameManager.P1Selected and not GameManager.P2Selected:
+			setText(tooltipText.SideSelection.P1IdleText, GameManager.currentP1Side)
+		elif not GameManager.P1Selected and GameManager.P2Selected:
+			setText(tooltipText.SideSelection.P2IdleText, GameManager.currentP2Side)
+		elif not GameManager.P1Selected and not GameManager.P2Selected:
+			setText(tooltipText.SideSelection.P1TooltipText, GameManager.PlayableSides.TOP)
+			setText(tooltipText.SideSelection.P2TooltipText, GameManager.PlayableSides.BOTTOM)
+	elif GameManager.currentGameState == GameManager.GameStates.ShipSelection:
+		if GameManager.currentP1Side == GameManager.PlayableSides.TOP:
+			setText(tooltipText.ShipSelection.P1TooltipText, GameManager.PlayableSides.TOP)
+			setText(tooltipText.ShipSelection.P2TooltipText, GameManager.PlayableSides.BOTTOM)
+		elif GameManager.currentP1Side == GameManager.PlayableSides.BOTTOM:
+			setText(tooltipText.ShipSelection.P2TooltipText, GameManager.PlayableSides.TOP)
+			setText(tooltipText.ShipSelection.P1TooltipText, GameManager.PlayableSides.BOTTOM)
 
 func playIntroAnim() -> void:
 	animPlayer.play("IntroAnim")
--- src/Scripts/Scenes/UI/TextTextureUI.gd
+++ src/Scripts/Scenes/UI/TextTextureUI.gd
@@ -1,34 +1,66 @@
 extends Control
 
-enum TextPositions { TOP, CENTER, BOTTOM }
+@onready var upperShineGradient = $UpperText/UpperText/ShineGradient
+@onready var lowerShineGradient = $LowerText/LowerText/ShineGradient
 
-@onready var upperText = $UpperTextContainer/UpperText
-@onready var middleText = $CenterTextContainer/MiddleText
-@onready var lowerText = $LowerTextContainer/LowerText
+@onready var upperText = $UpperText/UpperText
+@onready var lowerText = $LowerText/LowerText
 @onready var animPlayer = $AnimPlayer
+
+var UIText = UIUtil.Text["SelectionScene"].TextTextureUI
 
 func _ready() -> void:
 	if GameManager.currentGameState == GameManager.GameStates.SideSelection:
-		setText("TOP\nSIDE\nTOP\nSIDE", TextPositions.TOP)
-		setText("BOTTOM\nSIDE\nBOTTOM\nSIDE", TextPositions.BOTTOM)
+		setText(UIText.SideSelection.UpperText, GameManager.PlayableSides.TOP)
+		setText(UIText.SideSelection.LowerText, GameManager.PlayableSides.BOTTOM)
 	elif GameManager.currentGameState == GameManager.GameStates.ShipSelection:
 		if GameManager.currentP1Side == GameManager.PlayableSides.TOP:
-			setText("PLAYER 1\nPLAYER 1\nPLAYER 1\nPLAYER 1", TextPositions.TOP)
-			setText("PLAYER 2\nPLAYER 2\nPLAYER 2\nPLAYER 2", TextPositions.BOTTOM)
+			setText(UIText.ShipSelection.P1Text, GameManager.PlayableSides.TOP)
+			setText(UIText.ShipSelection.P2Text, GameManager.PlayableSides.BOTTOM)
 		elif GameManager.currentP2Side == GameManager.PlayableSides.BOTTOM:
-			setText("PLAYER 2\nPLAYER 2\nPLAYER 2\nPLAYER 2", TextPositions.TOP)
-			setText("PLAYER 1\nPLAYER 1\nPLAYER 1\nPLAYER 1", TextPositions.BOTTOM)
+			setText(UIText.ShipSelection.P2Text, GameManager.PlayableSides.TOP)
+			setText(UIText.ShipSelection.P1Text, GameManager.PlayableSides.BOTTOM)
 		else:
-			setText("PLAYER 1\nPLAYER 1\nPLAYER 1\nPLAYER 1", TextPositions.TOP)
-			setText("PLAYER 2\nPLAYER 2\nPLAYER 2\nPLAYER 2", TextPositions.BOTTOM)
+			setText(UIText.ShipSelection.P1Text, GameManager.PlayableSides.TOP)
+			setText(UIText.ShipSelection.P2Text, GameManager.PlayableSides.BOTTOM)
 
-func setText(_str: String, pos: TextPositions) -> void:
-	if pos == TextPositions.TOP:
+func setText(_str: String, pos: GameManager.PlayableSides) -> void:
+	if pos == GameManager.PlayableSides.TOP:
 		upperText.text = _str
-	elif pos == TextPositions.CENTER:
-		middleText.text = _str
-	elif pos == TextPositions.BOTTOM:
+	elif pos == GameManager.PlayableSides.BOTTOM:
 		lowerText.text = _str
 
 func playIntroAnim() -> void:
 	animPlayer.play("IntroAnim")
+	await animPlayer.animation_finished
+	animPlayer.play("idleScroll")
+
+func playShineAnim(pos: GameManager.PlayableSides) -> void:
+	var tween: Tween = create_tween()
+	if pos == GameManager.PlayableSides.TOP:
+		tween.tween_property(upperShineGradient, "position", Vector2(upperShineGradient.position.x, 512), 0.5).from(Vector2(upperShineGradient.position.x, -256))
+	elif pos == GameManager.PlayableSides.BOTTOM:
+		tween.tween_property(lowerShineGradient, "position", Vector2(lowerShineGradient.position.x, -256), 0.5).from(Vector2(lowerShineGradient.position.x, 512))
+	else: 
+		tween.kill()
+
+func sceneTextTextureHandler() -> void:
+	var tooltipText = UIText
+	if GameManager.currentGameState == GameManager.GameStates.SideSelection:
+		if GameManager.P1Selected and GameManager.P2Selected:
+			setText(tooltipText.ShipSelection.P1Text, GameManager.currentP1Side)
+			setText(tooltipText.ShipSelection.P2Text, GameManager.currentP2Side)
+		elif GameManager.P1Selected and not GameManager.P2Selected:
+			setText(tooltipText.ShipSelection.P1Text, GameManager.currentP1Side)
+		elif not GameManager.P1Selected and GameManager.P2Selected:
+			setText(tooltipText.ShipSelection.P2Text, GameManager.currentP2Side)
+		elif not GameManager.P1Selected and not GameManager.P2Selected:
+			setText(tooltipText.SideSelection.UpperText, GameManager.PlayableSides.TOP)
+			setText(tooltipText.SideSelection.LowerText, GameManager.PlayableSides.BOTTOM)
+	elif GameManager.currentGameState == GameManager.GameStates.ShipSelection:
+		if GameManager.currentP1Side == GameManager.PlayableSides.TOP:
+			setText(tooltipText.ShipSelection.P1Text, GameManager.PlayableSides.TOP)
+			setText(tooltipText.ShipSelection.P2Text, GameManager.PlayableSides.BOTTOM)
+		elif GameManager.currentP1Side == GameManager.PlayableSides.BOTTOM:
+			setText(tooltipText.ShipSelection.P2Text, GameManager.PlayableSides.TOP)
+			setText(tooltipText.ShipSelection.P1Text, GameManager.PlayableSides.BOTTOM)
