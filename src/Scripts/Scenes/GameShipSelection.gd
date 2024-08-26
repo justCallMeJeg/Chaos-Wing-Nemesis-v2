80cf48080b592d9c990a85cc2146939458e3e1b8
gjpgabayeron <gjpgabayeron@gmail.com>
2024-08-26 16:30:51 +0800

    Added timerIndicator and updated script

--- src/Scripts/Scenes/GameShipSelection.gd
+++ src/Scripts/Scenes/GameShipSelection.gd
@@ -6,14 +6,3 @@ extends Control
 func _ready():
 	print("[DEBUG]: GameShipSelection scene ready...")
 	animPlayer.play("IntroAnim")
-	
-func _input(event) -> void:
-	if event is InputEventKey and event.pressed:
-		match event.keycode:
-			KEY_SPACE:
-				if GameManager.currentGameState == GameManager.GameStates.SideSelection:
-					GameManager.currentGameState = GameManager.GameStates.ShipSelection
-					get_tree().change_scene_to_file("res://src/Scenes/GameStates/GameShipSelection.tscn")
-				else:
-					GameManager.currentGameState = GameManager.GameStates.SideSelection
-					get_tree().change_scene_to_file("res://src/Scenes/GameStates/GameSideSelection.tscn")
