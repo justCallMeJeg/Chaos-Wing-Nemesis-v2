cab63978a1ba3b83061dcf0cb3cf28421fd760b1
gjpgabayeron <gjpgabayeron@gmail.com>
2024-08-26 16:28:46 +0800

    Added ready state confirmation/removal support

--- src/Scripts/Scenes/UI/SideBorderUI.gd
+++ src/Scripts/Scenes/UI/SideBorderUI.gd
@@ -15,16 +15,30 @@ func playIntroAnim() -> void:
 		GameManager.GameStates.ShipSelection:
 			setSideText("P1 SELECT A SHIP!", BorderDir.Left)
 			setSideText("P2 SELECT A SHIP!", BorderDir.Right)
-	leftAnimPlayer.play("IntroAnim")
-	rightAnimPlayer.play("IntroAnim")
+	leftAnimPlayer.play("introAnim")
+	rightAnimPlayer.play("introAnim")
 
-func setReadyState(side: BorderDir) -> void:
-	if side == BorderDir.Left:
+func setReadyState(player: GameManager.Player) -> void:
+	if player == GameManager.Player.P1:
 		setSideText("PLAYER 1 READY!", BorderDir.Left)
-		leftAnimPlayer.play("PlayerReadyState")
-	else:
+		leftAnimPlayer.play("setReadyStateAnim")
+	elif player == GameManager.Player.P2:
 		setSideText("PLAYER 2 READY!", BorderDir.Right)
-		rightAnimPlayer.play("PlayerReadyState")
+		rightAnimPlayer.play("setReadyStateAnim")
+
+func removeReadyState(player: GameManager.Player) -> void:
+	if player == GameManager.Player.P1:
+		if GameManager.currentGameState == GameManager.GameStates.SideSelection:
+			setSideText("P1 SELECT A SIDE!", BorderDir.Left)
+		elif GameManager.currentGameState == GameManager.GameStates.ShipSelection:
+			setSideText("P1 SELECT A SHIP!", BorderDir.Left)
+		leftAnimPlayer.play("removeReadyStateAnim")
+	elif player == GameManager.Player.P2:
+		if GameManager.currentGameState == GameManager.GameStates.SideSelection:
+			setSideText("P2 SELECT A SIDE!", BorderDir.Right)
+		elif GameManager.currentGameState == GameManager.GameStates.ShipSelection:
+			setSideText("P2 SELECT A SHIP!", BorderDir.Right)
+		rightAnimPlayer.play("removeReadyStateAnim")
 
 func setSideText(_str: String, side: BorderDir) -> void:
 	if side == BorderDir.Left:
