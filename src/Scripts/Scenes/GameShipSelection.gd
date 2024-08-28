5b89c79747f7edd16743bf975fff310950f93a6e
Rover <87892483+justCallMeJeg@users.noreply.github.com>
2024-08-28 15:31:01 +0800

    Fixed textTexture issue not showing updated data

--- src/Scripts/Scenes/GameShipSelection.gd
+++ src/Scripts/Scenes/GameShipSelection.gd
@@ -1,8 +1,13 @@
 extends Control
 
+@onready var textTextureHandler = $TextTextureUI
+@onready var sideTooltipHandler = $SideTooltipUI
+
 @onready var animPlayer = $AnimPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	textTextureHandler.sceneTextTextureHandler()
+	sideTooltipHandler.sceneTooltipHandler()
 	print("[DEBUG]: GameShipSelection scene ready...")
 	animPlayer.play("IntroAnim")
